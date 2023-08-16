5d03ceb97665210a36cf38fb6d1c5c79e3f82d36
wesley1248 <wesleytms.es@gmail.com>
2023-08-16 19:58:20 -0300

    up

---
+++ StringBuilder/Metodos/Reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revere {
+    public static class Reverse {
+        public static string ReverterEntrada(string entrada)
+        {
+            string[] palavras = entrada.Split(' ');
+            string primeira = palavras[0];
+            string segunda = palavras[1];
+            string terceira = palavras[2];
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(segunda);
+            sb.Append(" ");
+            sb.AppendLine(primeira);
+            sb.Append(" ");
+            sb.AppendLine(terceira);
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ StringBuilder/Program.cs
@@ -0,0 +1,20 @@
+﻿using Revere;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.Write("digite duas palavras: ");
+            string entrada = Console.ReadLine();
+            string resultado = Reverse.ReverterEntrada(entrada);
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
